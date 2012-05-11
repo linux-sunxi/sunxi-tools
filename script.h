a14b0a7aa810b597a35bd2a3aa16599ba1217c48
Alejandro Mery <amery@geeks.cl>
2012-05-11 16:20:48 +0200

    script_bin: move bin structs from script.h to script_bin.h

--- script.h
+++ script.h
@@ -75,37 +75,6 @@ struct script_gpio_entry {
 	int32_t data[4];
 };
 
-/** binary representation of the head of a section */
-struct script_bin_section {
-	char name[32];
-	int32_t length;
-	int32_t offset;
-};
-
-/** binary representation of the head of the script file */
-struct script_bin_head {
-	int32_t sections;
-	int32_t version[3];
-	struct script_bin_section section[];
-};
-
-/** binary representation of the head of an entry */
-struct script_bin_entry {
-	char name[32];
-	int32_t offset;
-	int32_t pattern;
-};
-
-/** binary representation of a GPIO */
-struct script_bin_gpio_value {
-	int32_t port;
-	int32_t port_num;
-	int32_t mul_sel;
-	int32_t pull;
-	int32_t drv_level;
-	int32_t data;
-};
-
 /** create a new script tree */
 struct script *script_new(void);
 /** deletes a tree recursively */
--- script_bin.h
+++ script_bin.h
@@ -17,6 +17,37 @@
 #ifndef _SUBXI_TOOLS_SCRIPT_BIN_H
 #define _SUBXI_TOOLS_SCRIPT_BIN_H
 
+/** binary representation of the head of a section */
+struct script_bin_section {
+	char name[32];
+	int32_t length;
+	int32_t offset;
+};
+
+/** binary representation of the head of the script file */
+struct script_bin_head {
+	int32_t sections;
+	int32_t version[3];
+	struct script_bin_section section[];
+};
+
+/** binary representation of the head of an entry */
+struct script_bin_entry {
+	char name[32];
+	int32_t offset;
+	int32_t pattern;
+};
+
+/** binary representation of a GPIO */
+struct script_bin_gpio_value {
+	int32_t port;
+	int32_t port_num;
+	int32_t mul_sel;
+	int32_t pull;
+	int32_t drv_level;
+	int32_t data;
+};
+
 size_t script_bin_size(struct script *script,
 		       size_t *sections, size_t *entries);
 
