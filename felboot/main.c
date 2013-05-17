de093ee7eff417e188b3c60c1dc1d574c9a546ac
Henrik Nordstrom <henrik@henriknordstrom.net>
2013-05-17 16:07:54 +0200

    felboot: Reuse sunxi_board_init() directly from u-boot tree

--- felboot/main.c
+++ felboot/main.c
@@ -24,8 +24,11 @@ void _start(void)
 	clock_init();
 	gpio_init();
 	uart_init();
+	timer_init();
 	s_init();
 }
+
+int gdata;
 
 void dcache_enable(void)
 {
@@ -52,45 +55,6 @@ void udelay(unsigned long usec)
 	__udelay(usec);
 }
 
-void sunxi_board_init(void)
-{
-	int power_failed = 1;
-	int ramsize;
-
-	timer_init();
-
-	printf("DRAM:");
-	ramsize = sunxi_dram_init();
-	if (!ramsize) {
-		printf(" ?");
-		ramsize = sunxi_dram_init();
-	}
-	if (!ramsize) {
-		printf(" ?");
-		ramsize = sunxi_dram_init();
-	}
-	printf(" %dMB\n", ramsize>>20);
-	if (!ramsize)
-		hang();
-
-#ifdef CONFIG_AXP209_POWER
-	power_failed |= axp209_init();
-	power_failed |= axp209_set_dcdc2(1400);
-	power_failed |= axp209_set_dcdc3(1250);
-	power_failed |= axp209_set_ldo2(3000);
-	power_failed |= axp209_set_ldo3(2800);
-	power_failed |= axp209_set_ldo4(2800);
-#endif
-
-	/*
-	 * Only clock up the CPU to full speed if we are reasonably
-	 * assured it's being powered with suitable core voltage
-	 */
-	if (!power_failed)
-		clock_set_pll1(1008000000);
-}
-
-
 int putchar(int ch)
 {
 	return uart_putc(ch);
@@ -99,4 +63,14 @@ int putchar(int ch)
 int puts(const char *str)
 {
 	return uart_puts(str);
+}
+
+int sunxi_mmc_init(void)
+{
+	return -1;
+}
+
+int status_led_set(void)
+{
+	return -1;
 }
