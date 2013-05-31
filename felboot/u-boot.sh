0be7b52c530d3d849410c856dff3a3474c1e9013
Henrik Nordstrom <henrik@henriknordstrom.net>
2013-05-31 23:28:59 +0200

    felboot have been fully merged into sunxi u-boot

--- felboot/u-boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -ex
-fel write 0x2000 fel-boot-${1}.bin
-fel exe 0x2000
-fel write 0x4a000000 ../u-boot.bin
-fel exe 0x4a000000
--- felboot/usb-boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -ex
-fel write 0x2000 fel-boot-${1}.bin
-fel exe 0x2000
-fel write 0x4a000000 ../u-boot.bin
-fel write 0x43000000 ../script.bin
-fel write 0x44000000 ../uImage
-fel write 0x4c000000 ../initramfs.img
-fel exe 0x4a000000
