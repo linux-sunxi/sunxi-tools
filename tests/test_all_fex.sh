b106aeb310fed71b3f628ce6abf7e0f866aa3043
Bernhard Nortmann <bernhard.nortmann@web.de>
2016-10-29 14:47:28 +0200

    appveyor: Work around incorrect "find" invocation
    test_all_fex.sh now accepts an optional second parameter that
    specifies the find executable to use. Without explicitly passing
    it, AppVeyor would invoke the wrong one (Windows' own find.exe).
    
    Signed-off-by: Bernhard Nortmann <bernhard.nortmann@web.de>

--- tests/test_all_fex.sh
+++ tests/test_all_fex.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 FEXFILES=fexfiles.lst
-find $1 -name '*.fex' > ${FEXFILES}
+if test -z $2; then
+	FIND=find
+else
+	FIND=$2
+fi
+${FIND} $1 -name '*.fex' > ${FEXFILES}
 while read fex; do
 	./fextest.sh ${fex} || exit
 done <${FEXFILES}
