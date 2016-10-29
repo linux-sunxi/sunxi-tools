90070ac822e2c7b7beb5ad36939159d242d39dea
Bernhard Nortmann <bernhard.nortmann@web.de>
2016-10-29 11:21:44 +0200

    Prepare v1.4.1 tag
    For now, this is for informational purposes (program version
    output), but it might also serve as an anchor point for a
    "bugfix" release after some changes made to the build system.
    
    Signed-off-by: Bernhard Nortmann <bernhard.nortmann@web.de>

--- autoversion.sh
+++ autoversion.sh
@@ -4,7 +4,7 @@
 # output (on stdout) can easily be redirected to a file.
 #
 
-LATEST_RELEASE="v1.4"
+LATEST_RELEASE="v1.4.1"
 
 if VER=`git describe --tags --dirty --always`; then
 	echo "Setting version information: ${VER}" >&2
